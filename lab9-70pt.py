acc64c0079a1b8ecc21e595406be1742630d9d35
noaheli <noah.eli@gmail.com>
2014-10-15 14:59:11 -0500

    70 points

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -12,3 +12,10 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+print "Please insert a temperature (Celcius)"
+userinput = int(raw_input())
+userinput = userinput * 9
+userinput = userinput / 5
+userinput = userinput + 32
+print "Here is the Fahrenheighttemperature."
+print userinput
